d7901987201d918ceb8baa1ac323a1c9912f88b2
jose-valdez_IMSSB <jose-valdez_IMSSB@users.noreply.github.com>
2026-09-10 17:27:08 -0600

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,7 @@ function aistudioMediaPlugin(): Plugin {
 
 export default defineConfig(() => {
   return {
+    base: '/garrafon/',
     plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
     resolve: {
       alias: {
